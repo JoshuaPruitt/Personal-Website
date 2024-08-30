992be2d7092f8acfd46b28633621507e6ef2b9be
Joshua <joshuapruitt6484@gmail.com>
2024-08-30 15:06:45 -0700

    made changes to navBar.jsx for page rendering

--- Challenge-12-vite/src/components/navBar.jsx
+++ Challenge-12-vite/src/components/navBar.jsx
@@ -1,6 +1,5 @@
 import '../style/header.css'
-import React, { useEffect } from 'react'
-import App from '../App.jsx'
+import React, { useEffect, useState } from 'react'
 
 const actions = [
     {name: 'aboutMe', type: 'b1' },
@@ -9,21 +8,50 @@ const actions = [
     {name: 'resume', type: 'b4'},
 ]
 
-function Header(){
-    const [content, setPage] = React.useState('b1')
+function navBar(){
+    let pageInfo
+    const [content, setPage] = useState('b1')
+
+
+    if (content == "aboutMe"){
+        pageInfo = <div>
+                    <h2>Layout 1</h2>
+                    {/* Content for layout 1 */}
+                   </div>
+    } else if (content == "portfolio"){
+        pageInfo = <div>
+                    <h2>Layout 2</h2>
+                    {/* Content for layout 1 */}
+                   </div>
+
+    } else if (content == "contactMe"){
+        pageInfo = <div>
+                    <h2>Layout 3</h2>
+                    {/* Content for layout 1 */}
+                   </div>
+
+    } else {
+        pageInfo = <div>
+                    <h2>Layout 0</h2>
+                    {/* Content for layout 1 */}
+                   </div>
+    }
 
     return (
         <>
             <div>
+                {/* map out all buttons on the nav bar*/}
                 {actions.map((action) => (
-                    <button key={action.type} onClick={() => setPage(App(action.type))}>
+                    <button key={action.type} onClick={() => setPage(action.name)}>
                     {action.name}
                 </button>
                 ))}
+
+            {pageInfo}
             </div>
         </>
     )
     
 }
 
-export default Header
+export default navBar
