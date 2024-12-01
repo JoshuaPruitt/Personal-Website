abca42f486d8af0ff9c155dd2a12cd0d98cfb382
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-11-30 18:28:09 -0800

    Major styling changes to update page for use

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import pageData from "./pages/index.ts"
 import emailJs from '@emailjs/browser';
 import './style/App.css'
 
-import { FaBook } from "react-icons/fa";
-import { AiOutlineBranches } from "react-icons/ai";
-import { IoMdPerson, IoIosMail } from "react-icons/io";
 
 const service_id = process.env.SERVICE_ID || "";
 const template_id = process.env.TEMPLATE_ID || "";
@@ -40,20 +37,10 @@ function App(){
 
     return (
         <>
-            <header>
-                <div>
-                  <h1 id='logo'>Joshua Pruitt</h1>
-                </div>
-
-                <nav>
-                  <a href='#'><span><IoMdPerson/></span> About Me</a>
-                  <a href='#resume'><span><FaBook/></span> Resume</a>
-                  <a href='#portfolioContainer'><span></span><AiOutlineBranches /> Portfolio</a>
-                  <a href='#contact'><span><IoIosMail /></span> Contact Me</a>
-                </nav>
-            </header>
-
-            <body>
+            <main>
+              <div>
+                <h1 id='logo'>Joshua Pruitt</h1>
+              </div>
               {/*Will contain the about me section of the page */}
               <div>
                   {pageData.aboutMe()}
@@ -87,21 +74,19 @@ function App(){
                     <input id='submit' type='submit' value="Send Email"/>
                 </form>
               </div>
-            </body>
-                  
-            
-            
-            <footer id='bottomBar'>
-              <div id='linksbar'>
-                <a href='https://github.com/JoshuaPruitt'>
-                    <img src={githubLogo} width='50'height='50'></img>
-                </a>
 
-                <a href='https://www.linkedin.com/in/joshua-pruitt-1a494a311/'>
-                    <img src={linkedInLogo} width='50' height='50'></img>
-                </a>
-              </div>
-            </footer>
+              <footer>
+                <div id='linksbar'>
+                  <a href='https://github.com/JoshuaPruitt'>
+                      <img src={githubLogo} width='50'height='50' className='footerImg'></img>
+                  </a>
+
+                  <a href='https://www.linkedin.com/in/joshua-pruitt-1a494a311/'>
+                      <img src={linkedInLogo} width='50' height='50' className='footerImg'></img>
+                  </a>
+                </div>
+              </footer>
+            </main>    
         </>
     )   
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style/index.css'
 
+import { FaBook } from "react-icons/fa";
+import { AiOutlineBranches } from "react-icons/ai";
+import { IoMdPerson, IoIosMail } from "react-icons/io";
+
 
 //create the page body
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <nav>
+      <div>
+        <a href='#'><span><IoMdPerson/></span> About Me</a>
+        <a href='#resume'><span><FaBook/></span> Resume</a>
+        <a href='#portfolioContainer'><span><AiOutlineBranches /></span> Portfolio</a>
+        <a href='#contact'><span><IoIosMail /></span> Contact Me</a>
+      </div>
+    </nav>
     <App />
   </React.StrictMode>,
 )
+
 
 
 
