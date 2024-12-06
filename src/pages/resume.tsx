b45aec8bfa4f0b6be46a81ad2bd8bea8ec83b693
Joshua <joshuapruitt6484@gmail.com>
2024-12-06 09:57:02 -0800

    changed resume import to make it more compliant with netlify deployment

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,5 +1,5 @@
+import Resume from "../assets/Resume.pdf";
 import '../style/App.css'
-
 
 const resume = () => {
 
@@ -10,12 +10,12 @@ const resume = () => {
             </div>
 
             <div className="pdfViewer" style={{height: '85%'}}>
-                <embed src="../assets/Resume.pdf" width="500" height="685" type="application/pdf"></embed>
+                <embed src={Resume} width="500" height="685" type="application/pdf"></embed>
             </div>
 
 
             <div className='resume'>
-                <h3>Click the link <a href='../assets/Resume.pdf' download>here</a> to download my resume!</h3>
+                <h3>Click the link <a href={Resume} download>here</a> to download my resume!</h3>
             </div>
         </>
     )
