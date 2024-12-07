1dfc1119cb535255deb679b56343580a6194846b
Joshua <joshuapruitt6484@gmail.com>
2024-12-07 13:09:52 -0800

    begun changes to the portfolio part of the site

--- image.d.ts
+++ image.d.ts
@@ -4,4 +4,8 @@ declare module "*.jpeg";
 declare module "*.gif";
 declare module "*.svg";
 declare module "*.txt";
+
+//pdf files for resume and css files for widget
 declare module "*.pdf";
+declare module "*.css";
+declare module "*.min.js";
