796d57b00cff2d6f1bd16843eee532c1a7034391
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2025-09-28 14:48:22 -0700

    Removed all files to start from fresh on a site rewrite

--- src/interfaces/projectCard_Interface.tsx
+++
@@ -1,5 +0,0 @@
-
-export default interface Project {
-    link: string,
-    img: string,
-}
--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import aboutMe from "./aboutMe";
-import portfolio from "./portfolio";
-import resume from "./resume";
-
-export default {aboutMe, portfolio, resume};
